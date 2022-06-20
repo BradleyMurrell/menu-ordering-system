63dc2c1a7897c2481045b7570290933b19558241
BradleyMurrell <b.murrell89@gmail.com>
2022-06-20 19:51:08 +0000

    Add order page

--- customer/views.py
+++ customer/views.py
@@ -4,3 +4,7 @@ from django.views import View
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/index.html')
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/order.html')
